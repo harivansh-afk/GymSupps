cbcb3aa87e63cfdfca8e8185ef67974d06c6e64e
Harivansh Rathi <rathiharivansh@gmail.com>
2025-02-20 20:01:54 -0500

    hotfix: errors

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,5 +3,5 @@ import { ShopifyProductGrid } from '../components/ShopifyProductGrid';
 import { mockProducts } from '../data/mockShopifyProducts';
 
 export const ProductsPage: React.FC = () => {
-  return <ShopifyProductGrid products={mockProducts} />;
+    return <ShopifyProductGrid products={mockProducts} />;
 };
